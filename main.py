68a72088d7ba14acead7d9e70867340238580a93
Lukasz <lukasz.gibas96@gmail.com>
2026-08-02 23:08:46 +0200

    Add main application main loop

--- main.py
+++ main.py
@@ -1,27 +1,60 @@
 import csv
+import sys
 
 def main():
 
-    songs = load_songs_from_csv()
-
-    num_setlist = get_setlist_from_user()
-
-
-
-    setlist , setlist_duration = create_setlist(num_setlist, songs)
+    run_application()
     
 
-    print(setlist)
-    print(setlist_duration)
+    
+        
 
-    duration = convert_sec_to_time(setlist_duration)
-    print(f"Duration: {duration}")
-
+    
 
 
 
 # ---------------------- function definitions
 
+def run_application():
+
+    songs = load_songs_from_csv()
+    while True:
+    
+            show_menu()
+            menu_choice = get_menu_choice()
+            if menu_choice == 1:
+                num_setlist = get_setlist_from_user()
+    
+                setlist , setlist_duration = create_setlist(num_setlist, songs)
+        
+                print(setlist)
+                print(setlist_duration)
+                duration = convert_sec_to_time(setlist_duration)
+                print(f"Duration: {duration}")
+                
+            elif menu_choice == 2:
+                ...
+            elif menu_choice == 3:
+                ...
+            elif menu_choice == 0:
+                say_goodbye()
+                break 
+    
+    
+
+def show_menu():
+
+    print("""
+    ======================================
+              SETLIST GENERATOR
+    ======================================
+
+    1. Generate setlist.
+    2. Add song.
+    3. Show database.
+    0. Exit
+    ---------------------------------------
+    """)
 
 def load_songs_from_csv():
 
@@ -32,6 +65,9 @@ def load_songs_from_csv():
             for row in reader:
                 songs.append({"number": row["number"], "title": row["title"], "duration": row["duration"]})
     return songs
+
+def get_menu_choice():
+     return int(input("Choose action: "))
 
 def get_setlist_from_user():
     setlist = []
@@ -54,8 +90,7 @@ def create_setlist(user_setlist, dataset):
                 setlist.append({"number": row["number"], "title": row["title"]})
                 setlist_duration += int(convert_time_to_sec(row["duration"]))
     return setlist, setlist_duration
-
-     
+    
 def convert_time_to_sec(str_time):
      minutes, seconds = str_time.split(":")
      total = int(minutes) * 60 + int(seconds)
@@ -67,7 +102,13 @@ def convert_sec_to_time(sec_time):
 
     return f"{minutes}min {seconds}sec"
 
-    
+def say_goodbye():
+    print("""
+======================================
+      ALL DONE, SEE YOU NEXT TIME
+======================================
+                    """)
+
 
 if __name__ == "__main__":
     main()
