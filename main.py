edfa6ffbffc35b178cdbe1504654210030164956
Lukasz <lukasz.gibas96@gmail.com>
2026-08-04 22:12:21 +0200

    Refactor application flow and add basic add-song logic

--- main.py
+++ main.py
@@ -23,18 +23,14 @@ def run_application():
             show_menu()
             menu_choice = get_menu_choice()
             if menu_choice == 1:
-                num_setlist = get_setlist_from_user()
-    
-                setlist , setlist_duration = create_setlist(num_setlist, songs)
-        
-                duration = convert_sec_to_time(setlist_duration)
-                print(f"Final setlist:\n {setlist}")
-                print(f"Duration: {duration}")
+                generate_setlist(songs)
                 
             elif menu_choice == 2:
-                ...
+                add_song_to_database(songs)
+
             elif menu_choice == 3:
-                ...
+                show_database(songs)
+
             elif menu_choice == 0:
                 say_goodbye()
                 break 
@@ -89,7 +85,16 @@ def create_setlist(user_setlist, dataset):
                 setlist.append({"number": row["number"], "title": row["title"]})
                 setlist_duration += int(convert_time_to_sec(row["duration"]))
     return setlist, setlist_duration
-    
+
+def generate_setlist(songs):
+    num_setlist = get_setlist_from_user()
+        
+    setlist , setlist_duration = create_setlist(num_setlist, songs)
+            
+    duration = convert_sec_to_time(setlist_duration)
+    print(f"Final setlist:\n {setlist}")
+    print(f"Duration: {duration}")
+
 def convert_time_to_sec(str_time):
      minutes, seconds = str_time.split(":")
      total = int(minutes) * 60 + int(seconds)
@@ -107,6 +112,23 @@ def say_goodbye():
       ALL DONE, SEE YOU NEXT TIME
 ======================================
                     """)
+
+def show_database(songs):
+    print("===== CURRENT DATABASE =====")
+    for row in songs:
+        print(f'{row["number"]}. {row["title"]} - {row["duration"]}')
+    print("============================")
+
+def add_song_to_database(songs):
+
+    new_number = input("New song's number: ")
+    new_title = input("New song's title: ")
+    new_duration = input("Duration of new song: ")
+
+    with open("songs.csv", "a") as file:
+        writer = csv.DictWriter(file)
+        writer.writerow({"number": new_number, "title": new_title, "duration": new_duration})
+
 
 
 if __name__ == "__main__":
