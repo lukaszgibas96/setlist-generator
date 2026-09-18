5b17925ee159672953801950990a6a8f7d36028b
Lukasz <lukasz.gibas96@gmail.com>
2026-09-18 22:17:56 +0200

    Add two-step song removal with confirmation screen

--- app.py
+++ app.py
@@ -7,14 +7,12 @@ app = Flask(__name__)
 app.secret_key = "temp"
 
 @app.route("/songs")
-
 def load_songs():
     songs = load_songs_from_csv()
     return render_template("songs.html",songs = songs)
 
 
 @app.route("/add_song", methods= ["GET", "POST"])
-
 def add_song():
     if request.method == "POST":
         songs = load_songs_from_csv()
@@ -43,12 +41,13 @@ def add_song():
         return render_template("add_song.html", number= "", title= "", duration= "")
 
 @app.route("/edit_song/<number>", methods= ["POST", "GET"])
-
 def edit_song(number):
 
     songs = load_songs_from_csv()
     if request.method == "GET":
-        current_title, current_duration = find_song_by_number(number, songs)
+        song = find_song_row_by_number(number, songs)
+        current_title = song["title"]
+        current_duration = song["duration"]
         return render_template("edit_song.html", number= number, title= current_title, duration= current_duration)
 
     elif request.method == "POST":
@@ -64,20 +63,44 @@ def edit_song(number):
                 return render_template("edit_song.html", error= "Invalid song duration format. Please, use mm:ss format.", number = number, title= new_title, duration= new_duration)
         else:
             return render_template("edit_song.html", error= "Please, use title w/o polish characters", number = number,title = new_title, duration= new_duration)
-@app.route("/home")
 
+@app.route("/home")
 def home():
     return render_template("home.html")
 
+@app.route("/remove", methods= ["GET", "POST"])
+def remove_song():
+    songs = load_songs_from_csv()
+    if request.method == "GET":
+        return render_template("remove.html", remove_number="")
+    else:
+        number = request.form["remove_number"]
+        if song_number_exists(number,songs):
+            return redirect(f"/remove/{number}")
+        else:
+            return render_template("remove.html", error = f"Song {number} does not exist in database.")
+
+@app.route("/remove/<number>", methods=["GET","POST"])
+def confirm_remove_song(number):
+    songs = load_songs_from_csv()
+    song= find_song_row_by_number(number, songs)
+    if request.method == "GET":
+        return render_template("remove_confirm.html", number= number, title= song["title"])
+    elif request.method == "POST":
+        songs.remove(song)
+        save_songs_to_CSV(songs)
+        flash(f'Song "{number} - {song["title"]}" successfully REMOVED from database!')
+        return redirect("/songs")
+    
+        
+
 # ----------------- auxiliary functions -----------------
 
-def find_song_by_number(number,database):
+def find_song_row_by_number(number,database):
 
     for song in database:
         if song["number"] == str(number):
-            current_title = song["title"]
-            current_duration = song["duration"]
-            return current_title, current_duration
+            return song
         
 
 
